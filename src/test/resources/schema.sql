baf16c2120d71012f4325b2b947a167175aef6ca
Adnane ZIANE <adnane.ziane@arhs-cube.com>
2023-12-21 15:36:16 +0100

    version 5

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS  users (
 	user_id integer PRIMARY KEY auto_increment,
 	image varchar(255) NULL,
 	name varchar(255) NULL
 );
 
-CREATE TABLE tasks (
+CREATE TABLE IF NOT EXISTS  tasks (
 	status bool NULL,
 	task_id integer PRIMARY KEY auto_increment,
 	user_id integer NULL,
